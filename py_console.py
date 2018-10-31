1e0cf6d5bad862f27cdd733ddf64c182a9fa8a14
mitcom <pawel@burniak.pl>
2018-10-31 21:08:20 +0100

    Add context manager which breaks every line

--- py_console.py
+++ py_console.py
@@ -14,16 +14,17 @@ with ansi.terminal() as terminal:
     for t in range(100):
         terminal.go_to_origin()
         for y in range(LINES):
-            for x in range (12):
-                terminal.write(
-                    PIXEL_UP,
-                    (int(y * 256/12), min(0+t*4, 255), int(x * 256/12)),
-                )
-            terminal.break_line()
-            for x in range(12):
-                terminal.write(
-                    PIXEL_DOWN,
-                    (int(y * 256/12), min(0+t*4, 255), int(x * 256/12)),
-                )
-            terminal.break_line()
+            with terminal.line():
+                for x in range (12):
+                    terminal.write(
+                        PIXEL_UP,
+                        (int(y * 256/12), min(0+t*4, 255), int(x * 256/12)),
+                    )
+
+            with terminal.line():
+                for x in range(12):
+                    terminal.write(
+                        PIXEL_DOWN,
+                        (int(y * 256/12), min(0+t*4, 255), int(x * 256/12)),
+                    )
         time.sleep(0.05)
